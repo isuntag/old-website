0a042bd753521232c68b1c4bf82cd21e33a4b7e9
Isaac <isuntag@gmail.com>
2018-01-18 20:41:41 -0500

    Removed fade on project div hover

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('body').removeClass('fade-out');
-    $(".project").click(function(){
-        window.open($(this).find("a:first").attr("href"), '_blank');
+    $('.project').click(function(){
+        window.open($(this).find('a:first').attr('href'), '_blank');
         return false;
     });
     var isTouchDevice = 'ontouchstart' in document.documentElement;
@@ -11,14 +11,14 @@ $(document).ready(function() {
             $(this).fadeTo('fast', 1)
         }).mouseout(function() {
             $(this).fadeTo('fast', 0.5)
-        })
+        });
     }
     if (isTouchDevice == true) {
         $('.scroll_button').on('touchstart', function() {
             $(this).fadeTo('fast', 1)
         }).on('touchend', function() {
             $(this).fadeTo('fast', 0.5)
-        })
+        });
     }
     $('.scroll_button').on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
